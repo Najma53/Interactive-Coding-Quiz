6a2a7c7acab09edc5a4e99061ee6fa823f29e445
Najma53 <najma.alibhai@gmail.com>
2022-12-05 11:39:35 +0000

    added quiz questions to questions.js file along with part pseudocoding

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+//questions are taken from W3 School website
+
+// Pseudocoding
+// upon the click button on index.html page, this questions hould start sisplaying
+//hence need to write a function regarding this , may be in logic.js file
+
+var questions = [
+    
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        options: ["<js>", "<script>","<javascript>","<scripting>"],
+        answer: "<script>"
+    },
+
+    {
+        question: "How to write an IF statement in JavaScript?",
+        options: ["if i == 5 then", "if i = 5 then", "if i = 5", "if (i == 5)"],
+        answer: "if (i == 5)"
+    },
+
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        options: ["rnd(7.25)", "round(7.25)", "Math.round(7.25)","Math.rnd(7.25)" ],
+        answer: "Math.round(7.25)"
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        options: ["onclick", "onmouseclick", "onchange","onmouseover"],
+        answer: "onclick"
+    }
+
+]
