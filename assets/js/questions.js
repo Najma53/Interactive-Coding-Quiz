f8f9ecccdfea6ee8bd93c75ce2d14184ebff6587
Najma53 <najma.alibhai@gmail.com>
2022-12-06 10:33:05 +0000

    rearranged the format of questions in jsquestion file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,45 @@
 //questions are taken from W3 School website
 
 // Pseudocoding
-// upon the click button on index.html page, this questions hould start sisplaying
+// upon the click button on index.html page, this questions should start displaying
 //hence need to write a function regarding this , may be in logic.js file
 
 var questions = [
     
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        options: ["<js>", "<script>","<javascript>","<scripting>"],
-        answer: "<script>"
+        choices1: "<js>",
+        choices2: "<script>",
+        choices3:"<javascript>",
+        choices4:"<scripting>",
+        correct: "<script>"
     },
 
     {
         question: "How to write an IF statement in JavaScript?",
-        options: ["if i == 5 then", "if i = 5 then", "if i = 5", "if (i == 5)"],
-        answer: "if (i == 5)"
+        choices1: "if i == 5 then",
+        choices2: "if i = 5 then", 
+        choices3:"if i = 5",
+        choices4: "if (i == 5)",
+        correct: "if (i == 5)"
     },
 
     {
         question: "How do you round the number 7.25, to the nearest integer?",
-        options: ["rnd(7.25)", "round(7.25)", "Math.round(7.25)","Math.rnd(7.25)" ],
-        answer: "Math.round(7.25)"
+        choices1: "rnd(7.25)",
+        choices2: "round(7.25)", 
+        choices3:"Math.round(7.25)",
+        choices4:"Math.rnd(7.25)",
+        correct: "Math.round(7.25)"
     },
 
     {
         question: "Which event occurs when the user clicks on an HTML element?",
-        options: ["onclick", "onmouseclick", "onchange","onmouseover"],
-        answer: "onclick"
+        choices: "onclick",
+        choices2:"onmouseclick",
+        choices3:"onchange",
+        choices4:"onmouseover",
+        correct: "onclick"
     }
 
-]
+];
